51ea3ab7466445165dc7442e4b9b3021a5fb155e
narayani-org <connect@narayani.org>
2026-08-27 15:03:18 +0530

    W

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -125,6 +125,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={`${orbitron.variable} ${inter.variable}`}>
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18397257443"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18397257443');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
